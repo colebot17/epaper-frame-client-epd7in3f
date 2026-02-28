845a96833b4ca9dee2da14cd52947a531015d09e
Colebot <70764431+colebot17@users.noreply.github.com>
2026-02-28 16:53:48 -0500

    Call enable.sh

--- update.sh
+++ update.sh
@@ -7,4 +7,4 @@ echo "Updating epaper application..."
 git fetch && git reset --hard origin/main
 $APP_DIR/venv/bin/pip install -r $APP_DIR/requirements.txt
 chmod +x enable.sh disable.sh update.sh
-sudo systemctl restart epaper
+$APP_DIR/enable.sh
