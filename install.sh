3bd6660bf87d4ec269ab22c640e162cbc3e1ee60
Colebot <70764431+colebot17@users.noreply.github.com>
2026-03-01 03:26:02 -0500

    chmod

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ echo "Installing Python dependencies..."
 $APP_DIR/venv/bin/pip install --upgrade pip
 $APP_DIR/venv/bin/pip install -r $APP_DIR/requirements.txt
 
-chmod +x enable.sh disable.sh update.sh
+chmod +x enable.sh disable.sh update.sh install_hotspot.sh
 
 ./install_hotspot.sh
 
--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ echo "Updating epaper application..."
 
 git fetch && git reset --hard origin/main
 $APP_DIR/venv/bin/pip install -r $APP_DIR/requirements.txt
-chmod +x enable.sh disable.sh update.sh
+chmod +x enable.sh disable.sh update.sh install_hotspot.sh
 $APP_DIR/enable.sh
 
 $APP_DIR/install_hotspot.sh
