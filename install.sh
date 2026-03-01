99cbc5762ea787e64727135e2f43c8e3abbf9583
Colebot <70764431+colebot17@users.noreply.github.com>
2026-03-01 13:29:06 -0500

    Remove remaining hotspot code

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ APP_DIR=$(pwd)
 echo "Updating system packages..."
 sudo apt update
 sudo apt install -y python3 python3-venv python3-pip python3-pil python3-numpy
-sudo systemctl disable --now hostapd
 
 echo "Creating virtual environment..."
 rm -rf venv
@@ -15,7 +14,7 @@ echo "Installing Python dependencies..."
 $APP_DIR/venv/bin/pip install --upgrade pip
 $APP_DIR/venv/bin/pip install -r $APP_DIR/requirements.txt
 
-chmod +x enable.sh disable.sh update.sh install_hotspot.sh
+chmod +x enable.sh disable.sh update.sh
 
 # enable main epaper.service
 ./enable.sh
